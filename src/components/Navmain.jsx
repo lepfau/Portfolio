7b86885ea6d0ca0f0ca11b8008fae36e7116e6a3
lepfau <edouard.pfauwadel@gmail.com>
2021-02-16 15:55:53 +0100

    understood activeclassnname

--- src/components/Navmain.jsx
+++ src/components/Navmain.jsx
@@ -4,17 +4,17 @@ import { NavLink } from 'react-router-dom';
 export default function Navmain() {
     return (
         <div>
-                      <nav>
-                      <NavLink className="navtext" exact to="/">
+                      <nav className="nav nav-pills nav-fill navtext">
+                      <NavLink className="nav-item" activeClassName="nav-link active" exact to="/">
                 Home
               </NavLink>
-              <NavLink className="navtext" exact to="/About">
+              <NavLink className="nav-item" activeClassName="nav-link active" exact to="/About">
                 About
               </NavLink>
-              <NavLink className="navtext" exact to="/Projects">
+              <NavLink className="nav-item" activeClassName="nav-link active" exact to="/Projects">
                 My projects
               </NavLink>
-              <NavLink  className="navtext" exact to="/Contact">
+              <NavLink  className="nav-item"  activeClassName="nav-link active"exact to="/Contact">
                 Contact
               </NavLink>
             </nav>
