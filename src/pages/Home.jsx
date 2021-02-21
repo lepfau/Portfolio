22b83095d20e7a66443975f20bba641f66fad36a
lepfau <edouard.pfauwadel@gmail.com>
2021-02-21 17:01:50 +0100

    na

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,53 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div className="imghome">
-                <h1 className="title">Edouard Pfauwadel</h1>
-                <h2 className="subtitle">Full Stack Web Developper</h2>
-                <nav className="nav nav-pills nav-fill navtext">
-                <NavLink className="nav-item" activeClassName="nav-link active" exact to="/">
-                Home
-              </NavLink>
-              <NavLink className="nav-item" activeClassName="nav-link active" exact to="/About">
-                About
-              </NavLink>
-              <NavLink className="nav-item" activeClassName="nav-link active" exact to="/Projects">
-                My projects
-              </NavLink>
-              <NavLink  className="nav-item"  activeClassName="nav-link active"exact to="/Contact">
-                Contact
-              </NavLink>
-                </nav>
-         </div>
-        )
-    }
+  render() {
+    return (
+      <div className="imghome">
+      <div id="stars"> </div>
+      <div id="stars2"> </div>
+      <div id="stars3"> </div>
+
+<div id="title"></div>
+        <h1 className="title">Edouard Pfauwadel</h1>
+        <hr></hr>
+        <h2 className="subtitle">Full Stack Web Developper</h2>
+        <nav className="nav nav-pills nav-fill navtext">
+          <NavLink
+            className="nav-item"
+           
+            exact
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className="nav-item"
+            activeClassName="nav-link active"
+            exact
+            to="/About"
+          >
+            About
+          </NavLink>
+          <NavLink
+            className="nav-item"
+            activeClassName="nav-link active"
+            exact
+            to="/Projects"
+          >
+            My projects
+          </NavLink>
+          <NavLink
+            className="nav-item"
+            activeClassName="nav-link active"
+            exact
+            to="/Contact"
+          >
+            Contact
+          </NavLink>
+        </nav>
+      </div>
+    );
+  }
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class Projects extends Component {
     render() {
         return (
-            <div>
+            <div className="projectsPage">
                 <h1>My projects</h1>
-
+<p>Super Typer 2077</p>
+<p>Pickit!</p>
+<p>Instagreen</p>
             </div>
         )
     }
